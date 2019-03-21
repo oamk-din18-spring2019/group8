1c5161b1d110d2bbea62ebb89c1cada86ed82785
Joel Ala-aho <joel.ala-aho@netti.fi>
2019-03-21 13:58:39 +0200

    Added login controller

---
+++ application/views/login/login_form.php
@@ -0,0 +1,8 @@
+<form class="" action="<?php echo site_url("login/login"); ?>" method="post">
+    <label for="">username</label> <br>
+    <input type="text" name="username" value=""> <br>
+    <label for="">password</label> <br>
+    <input type="text" name="password" value=""> <br>
+    <input type="submit" name="" value="login">
+ 
+</form>
