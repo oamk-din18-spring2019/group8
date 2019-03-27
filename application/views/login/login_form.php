493010cb85ed8b6a78ae9600337de2159b51d5e3
Joel Ala-aho <joel.ala-aho@netti.fi>
2019-03-27 10:05:49 +0200

    Added login form and js&css files

--- application/views/login/login_form.php
+++ application/views/login/login_form.php
@@ -3,6 +3,5 @@
     <input type="text" name="username" value=""> <br>
     <label for="">password</label> <br>
     <input type="text" name="password" value=""> <br>
-    <input type="submit" name="" value="login">
- 
+    <input type="submit" name="" value="login"> 
 </form>
---
+++ application/views/login/personal_page.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Welcome ".$_SESSION["username"];  
