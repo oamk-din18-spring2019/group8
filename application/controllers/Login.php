9ffa06fee5cf49e09751a128150c9922a30335c1
Joel Ala-aho <joel.ala-aho@netti.fi>
2019-03-29 09:07:11 +0200

    Login page now pushes register to a local database and finds users from it

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -14,14 +14,14 @@ class Login extends CI_Controller {
         $this->load->view("login/login_form", $data);
     }
 
-	public function Login()
+	public function login()
 	{
 		$this->load->model("Login_model");
         $given_username = $this->input->post("username");
         $given_password = $this->input->post("password");
 		$db_password=$this->Login_model->get_password($given_username);
 		
-        if(password_verify($given_password, $db_password))
+        if($given_password == $db_password) // <--- ONLY FOR TESTING !!! should be = password_verify($given_password, $db_password)
         {
             $_SESSION["logged_in"]=true;
             $_SESSION["username"]=$given_username;
@@ -31,7 +31,7 @@ class Login extends CI_Controller {
         else
         {
             $_SESSION["logged_in"]=false;
-            redirect("login");
+            redirect("");   
 		}
 		
 		function logout()
@@ -41,7 +41,47 @@ class Login extends CI_Controller {
             $data["page"]="login/logout_page";
             $this->load->view("menu/content", $data);
         }
-	}
+    }
+    
+    public function register()
+    {
+        $connection = mysqli_connect('localhost', 'root', 'root');
+        if (!$connection)
+        {
+            die("Database Connection Failed" . mysqli_error($connection));
+        }
+
+        $select_db = mysqli_select_db($connection, 'users');
+
+        if (!$select_db)
+        {
+            die("Database Selection Failed" . mysqli_error($connection));
+        }
+
+        $username = $_POST['username'];
+        $sql_usernames = "SELECT * FROM user WHERE username='$username'";
+        $res_usernames = mysqli_query($connection, $sql_usernames);
+  
+        if(mysqli_num_rows($res_usernames) > 0) 
+        {
+          $name_error = "Sorry... username already taken";
+          redirect("");
+        }
+
+        else if(isset($_POST['username']) && isset($_POST['password']))
+        {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+     
+            $query = "INSERT INTO `user` (username, password) VALUES ('$username', '$password')";
+            $result = mysqli_query($connection, $query);
+            if($result){
+                $smsg = "User Created Successfully.";
+            }else{
+                $fmsg ="User Registration Failed";
+            }
+        }
+    }
 
 
 }
