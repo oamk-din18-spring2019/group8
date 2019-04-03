190966d7d7370e7c98ae699c3f0039d08d843e10
Ossi <karja.ossi@gmail.com>
2019-04-03 10:29:21 +0300

    Added some testing stuff for the Battle page

---
+++ js/battle.js
@@ -0,0 +1,54 @@
+function battle_selections()
+{
+  var game1 = 0;
+  var game2 = 0;
+  var winner = "";
+  var checker = document.forms[battle_form].value;
+
+  if (document.getElementById('battle_value1').checked) {
+    game1++;
+  }
+  if (document.getElementById('battle_value2').checked) {
+    game2++;
+  }
+  if (document.getElementById('battle_value3').checked) {
+    game1++;
+  }
+  if (document.getElementById('battle_value4').checked) {
+    game2++;
+  }
+  if (document.getElementById('battle_value5').checked) {
+    game1++;
+  }
+  if (document.getElementById('battle_value6').checked) {
+    game2++;
+  }
+  if (document.getElementById('battle_value7').checked) {
+    game1++;
+  }
+  if (document.getElementById('battle_value8').checked) {
+    game2++;
+  }
+  if (document.getElementById('battle_value9').checked) {
+    game1++;
+  }
+  if (document.getElementById('battle_value10').checked) {
+    game2++;
+  }
+
+  if (game1 > game2)
+  {
+    winner = "game1";
+  }
+  else if (game2 > game1) {
+    winner = "game2";
+  }
+
+
+
+  console.log("game1 = "+game1);
+  console.log("game2 = "+game2);
+  console.log("winner is "+ winner);
+  console.log(checker);
+
+}
