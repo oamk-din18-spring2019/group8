8f77fd678496d6d790f67a42e5c27bf03dadec6c
Ossi <karja.ossi@gmail.com>
2019-04-09 12:09:10 +0300

    Just added the base profilepage

---
+++ application/models/Profile_model.php
@@ -0,0 +1,14 @@
+<?php
+
+defined("BASEPATH") or exit("No direct script access allowed");
+
+class Profile_model extends CI_model
+{
+    public function __construct()
+    {
+        parent:: __construct();
+    }
+
+
+
+}
--- application/views/menu/header.php
+++ application/views/menu/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <link rel = "stylesheet" href = "<?php echo base_url(); ?>css/style.css">
     <link rel = "stylesheet" href = "<?php echo base_url(); ?>css/battle.css">
+    <link rel = "stylesheet" href = "<?php echo base_url(); ?>css/profile.css">
     <link href="https://fonts.googleapis.com/css?family=Play" rel="stylesheet">
     <script type = 'text/javascript' src = "<?php echo base_url(); ?>js/battle.js"></script>
     <script type = 'text/javascript' src = "<?php echo base_url(); ?>js/sample.js"></script>
@@ -16,7 +17,7 @@
         </div>
         <div id="menu">
               <ul>
-                <li><a href="<#>">Profile</a> </li>
+                <li><a href="<?php echo site_url('profile'); ?>">Profile</a> </li>
                 <li><a href="<#>">Search</a> </li>
                 <li><a href="<?php echo site_url('battle'); ?>">Battle</a> </li>
                 <li><a href="<#>">Rankings</a> </li>
