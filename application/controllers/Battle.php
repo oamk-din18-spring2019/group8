b1d8d723f952683113fe80e0b615840744828d54
Ossi <karja.ossi@gmail.com>
2019-04-12 12:02:28 +0300

    Added randomizing for games.
    Joel did all the work.

--- application/controllers/Battle.php
+++ application/controllers/Battle.php
@@ -8,7 +8,7 @@ class Battle extends CI_Controller {
         parent::__construct();
 				if(!empty($_SESSION["logged_in"]) && $_SESSION["logged_in"] == true)
         {
-            //
+	            $this->load->model('Battle_model');
         }
         else
         {
@@ -17,10 +17,22 @@ class Battle extends CI_Controller {
 
 	}
 
+
 	function index()
     {
-        $data["page"]="battle/battle_page";
-        $this->load->view("menu/content", $data);
-    }
+			$game1=$this->Battle_model->pickedgames();
+			$game2=$this->Battle_model->pickedgames();
+			while($game1===$game2)
+				{
+						$game2=$this->Battle_model->pickedgames();
+				}
+			if($game1!=$game2)
+			{
+					$data["game1"]=$game1;
+					$data["game2"]=$game2;
+					$data["page"]="battle/battle_page";
+					$this->load->view("menu/content", $data);
+}
 
+}
 }
