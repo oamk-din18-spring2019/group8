5a035c79d8374f7e77bf5155c487f39c6936fac7
Ossi <karja.ossi@gmail.com>
2019-05-02 13:34:00 +0300

    Finalizing things.

--- application/views/games/get_game.php
+++ application/views/games/get_game.php
@@ -1,21 +1,11 @@
 <link rel = "stylesheet" href = "<?php echo base_url(); ?>css/style.css">
-
-<div class="gametable">
-    <table border="1">
-        <thead>
-            <tr><th>Name</th><th>Genre</th><th>Ranking</th><th>Genre Ranking</th><th>Wikipedia</th></tr>
-        </thead>
-        <tbody>
-            <?php foreach ($game as $row){
+<div class="games_gametable">
+           <?php foreach ($game as $row){
                 $link='https://en.wikipedia.org/wiki/';
-                echo "<tr>";
-                    echo "<td>".$row["game"]."</td>";
-                    echo "<td>".$row["genre"]."</td>";
-                    echo "<td>".$row["ranking"]."</td>";
-                    echo "<td>".$row["genreranking"]."</td>";
-                    echo "<td> <a target='_blank' href=\"https://en.wikipedia.org/wiki/" . $row["game"] . "\"> " . $row["game"] . " </a> </td>";
-                echo "</tr>";
+                    echo "<h1>".$row["game"]."</h1>";
+                    echo "<p><b>Genre: </b>".$row["genre"]."</p>";
+                    echo "<p><b>Overall rank: </b>".$row["ranking"]."</p>";
+                    echo "<p><b>Genre rank: </b>".$row["genreranking"]."</p>";
+                    echo "<p><b>Link to Wikipedia: </b> <a target='_blank' href=\"https://en.wikipedia.org/wiki/" . $row["game"] . "\"> " . $row["game"] . " </a> </p>";
             } ?>
-        </tbody>
-    </table>
 </div>
