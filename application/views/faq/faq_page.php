a75a3cc429b45d4d3fdcc0c67933c66510ff4113
Ossi Karjalainen <t8kaos00@students.oamk.fi>
2019-05-02 21:20:46 +0300

    Last changes.

--- application/views/faq/faq_page.php
+++ application/views/faq/faq_page.php
@@ -1,3 +1,28 @@
-<div class="">
-  <h1>FAQ & Info</h1>
+<div>
+  <h1 id="omegalul" style="display:flex; justify-content: center; text-align: center; width: 100%;">FAQ & Info</h1><br>
+
+<div style="text-align: center; width: 100%; font-size: 20px;">
+<p><b>What is the point of this website?</b></p>
+<p>Users will rate games based on their opinion. Then the games will be given ELO rating based on all user selections.</p><br>
+<p><b>Why can't I upload profile picture???</b></p>
+<p>It's a work in progress.</p><br>
+<p><b>What were the keybinds again?</b></p>
+<p>Left arrow selects the left option</p>
+<p>Right arrow selects the right option</p>
+<p>Up arrow clears selection if all selections are made</p>
+<p>Enter key will submit.</p><br>
+<p><b>Why won't my rank update?</b></p>
+<p>Because it's not an scheduled task, but rather we shall update them when ever we want =D.</p><br>
+<p><b>Where can I add games?</b></p>
+<p>You don't have enough power to add games, young one.</p><br>
+<p><b>Where can I change my password?</b></p>
+<p>You can't yet, it's a work in progress. Choose wisely.</p><br>
+<p><b>Which game is 1 and which is 2, when playing the minigame?</b></p>
+<p>Game 1 is on the left and Game 2 is on the right.</p><br>
+
+<p style="font-size: 30px;"><b>If you have something else to ask, send us email. You'll find our emails on the about page.</b></p>
+
+
+<br><br><br><br>
+</div>
 </div>
